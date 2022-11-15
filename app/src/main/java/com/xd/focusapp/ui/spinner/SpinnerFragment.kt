83cd8e41c2b0be5ba5343088ecbb239e5f2913b5
wodouxiangxiaole <85941647+wodouxiangxiaole@users.noreply.github.com>
2022-11-14 20:07:20 -0800

    rebuild spinner

--- app/src/main/java/com/xd/focusapp/ui/spinner/SpinnerFragment.kt
+++ app/src/main/java/com/xd/focusapp/ui/spinner/SpinnerFragment.kt
@@ -4,10 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.DecelerateInterpolator
+import android.view.animation.RotateAnimation
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.xd.focusapp.R
 import com.xd.focusapp.databinding.FragmentSpinnerBinding
+import java.util.*
 
 class SpinnerFragment : Fragment() {
 
@@ -16,6 +24,14 @@ class SpinnerFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    private lateinit var spin_button: Button
+    private lateinit var wheel: ImageView
+
+    private val sectors = arrayOf("common", "uncommon", "rare", "legendary")
+    private val sectorsDegree = arrayOf(0, 180, 288, 342)
+    private var degree:Int = 0
+    private var isSpinning:Boolean = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,12 +44,57 @@ class SpinnerFragment : Fragment() {
         _binding = FragmentSpinnerBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textSpinner
-        spinnerViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        spin_button = root.findViewById(R.id.spin_button)
+        wheel = root.findViewById(R.id.wheel)
+
+        spin_button.setOnClickListener {
+            if (!isSpinning) {
+                spin()
+                isSpinning = true
+            }
         }
         return root
     }
+
+    fun spin(){
+        var randomDegree = Random().nextInt(360)
+        val rotateAnimation = RotateAnimation(
+            0F, (360*sectors.size + 360-randomDegree).toFloat(),
+            RotateAnimation.RELATIVE_TO_SELF, 0.5f, RotateAnimation.RELATIVE_TO_SELF, 0.5f)
+        rotateAnimation.duration = 3600
+        rotateAnimation.fillAfter = true
+        rotateAnimation.setInterpolator(DecelerateInterpolator())
+        rotateAnimation.setAnimationListener(object: Animation.AnimationListener{
+            override fun onAnimationEnd(p0: Animation?) {
+                isSpinning = false
+                var res:String
+                if(randomDegree <= 180){
+                    res = "common"
+                }
+                else if(randomDegree <= 288){
+                    res = "uncommon"
+                }
+                else if(randomDegree <= 342){
+                    res = "rare"
+                }
+                else{
+                    res = "legendary"
+                }
+                Toast.makeText(requireActivity(),"You got " + res, Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onAnimationStart(p0: Animation?) {
+                println(randomDegree)
+            }
+
+            override fun onAnimationRepeat(p0: Animation?) {
+            }
+
+        })
+
+        wheel.startAnimation(rotateAnimation)
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
