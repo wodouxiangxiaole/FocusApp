5512c1fa3ed16ca89c68abc4cd62ef6bc33b9caa
Nicole Li <remever@hotmail.com>
2022-11-29 09:36:01 -0800

    Added service to the focus fragment

---
+++ app/src/main/java/com/xd/focusapp/ui/focus/FocusService.kt
@@ -0,0 +1,12 @@
+package com.xd.focusapp.ui.focus
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class FocusService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
