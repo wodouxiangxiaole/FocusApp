8058318a679483cc8632be77f8109288ca933f5c
lunaS112 <luna.sang21@gmail.com>
2022-12-05 01:45:48 -0800

    updated google login, setting switch user

--- app/src/main/java/com/xd/focusapp/ui/login/GoogleLoginActivity.kt
+++ app/src/main/java/com/xd/focusapp/ui/login/GoogleLoginActivity.kt
@@ -42,17 +42,17 @@ class GoogleLoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_google_login)
-        oneTapClient = Identity.getSignInClient(this)
+        //oneTapClient = Identity.getSignInClient(this)
         println("Debug: oneTapClient")
         firebaseAuth = Firebase.auth
         println("Debug: firebaseAuth")
         //just for testing purposes
 
-        if(firebaseAuth.currentUser != null){
-            println("Debug: firebaseAuth.currentUser = ${firebaseAuth.currentUser!!.email.toString()}")
-            firebaseAuth.signOut()
-            println("Debug: firebaseAuth.signOut ")
-        }
+//        if(firebaseAuth.currentUser != null){
+//            println("Debug: firebaseAuth.currentUser = ${firebaseAuth.currentUser!!.email.toString()}")
+//            firebaseAuth.signOut()
+//            println("Debug: firebaseAuth.signOut ")
+//        }
         //firebaseAuth.signOut()
 
 
@@ -60,32 +60,12 @@ class GoogleLoginActivity : AppCompatActivity() {
         progressDialog = ProgressDialog(this)
         progressDialog.setMessage("Google login ...")
         progressDialog.show()
-//        Configure the One Tap sign-in client
-//        https://developers.google.com/identity/one-tap/android/get-saved-credentials#1_configure_the_one_tap_sign-in_client
-//        signInRequest = BeginSignInRequest.builder()
-//            .setPasswordRequestOptions(BeginSignInRequest.PasswordRequestOptions.builder()
-//                .setSupported(true)
-//                .build())
-//            .setGoogleIdTokenRequestOptions(
-//                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
-//                    .setSupported(true)
-//                    // Your server's client ID, not your Android client ID.
-//                    .setServerClientId(getString(R.string.your_web_client_id))
-//                    // Only show accounts previously used to sign in.
-//                    .setFilterByAuthorizedAccounts(true)
-//                    .build())
-//            // Automatically sign in when exactly one credential is retrieved.
-//            .setAutoSelectEnabled(true)
-//            .build()
 
         // Configure Google Sign In
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.your_web_client_id))
+            .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .build()
-
-
-        //firebaseUser= firebaseAuth.currentUser!!
 
         googleSignInClient = GoogleSignIn.getClient(this, gso)
         println("Debug:googleSignInClient")
@@ -93,69 +73,44 @@ class GoogleLoginActivity : AppCompatActivity() {
 
     }
 
-//    override fun onStart() {
-//        super.onStart()
-//        // Check if user is signed in (non-null) and update UI accordingly.
-////        val currentUser = firebaseAuth.currentUser
-////        if(currentUser != null){
-////            firebaseUser = currentUser
-////        }
-//        val currentUser = firebaseAuth.currentUser
-//        updateUI(currentUser)
-//    }
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        if(firebaseAuth.currentUser != null) {
+            println("Debug: 1 ")
+            intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
 
-//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        super.onActivityResult(requestCode, resultCode, data)
-//
-//        when (requestCode) {
-//            REQ_ONE_TAP -> {
-//                try {
-//                    val credential = oneTapClient.getSignInCredentialFromIntent(data)
-//                    val idToken = credential.googleIdToken
-//                    when {
-//                        idToken != null -> {
-//                            // Got an ID token from Google. Use it to authenticate
-//                            // with Firebase.
-//                            println("Debug: idToken $idToken")
-//                            val firebaseCredential = GoogleAuthProvider.getCredential(idToken, null)
-//                            firebaseAuth.signInWithCredential(firebaseCredential).addOnCompleteListener(this) { task ->
-//                                    if (task.isSuccessful) {
-//                                        // Sign in success, update UI with the signed-in user's information
-//                                        Log.d(TAG, "signInWithCredential:success")
-//                                        Toast.makeText(this,"signInWithCredential:success", Toast.LENGTH_SHORT).show()
-//                                        val user = firebaseAuth.currentUser
-//                                        updateUI(user)
-//                                    } else {
-//                                        // If sign in fails, display a message to the user.
-//                                        Toast.makeText(this,"signInWithCredential:failure\n"+task.exception, Toast.LENGTH_SHORT).show()
-//                                        updateUI(null)
-//                                    }
-//                                }
-//                        }
-//
-//                        else -> {
-//                            // Shouldn't happen.
-//                            Toast.makeText(this,"No ID token", Toast.LENGTH_SHORT).show()
-//                        }
-//                    }
-//                }catch (e: Exception){
-//                    Toast.makeText(this,e.message, Toast.LENGTH_SHORT).show()
-//                }
-//            }
-//        }
-//    }
+    }
+
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
         // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
+        if (requestCode == 10001) {
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            println("Debug: 2 ")
             try {
                 // Google Sign In was successful, authenticate with Firebase
                 val account = task.getResult(ApiException::class.java)!!
                 Toast.makeText(this,"signInWithCredential:success", Toast.LENGTH_SHORT).show()
-                firebaseAuthWithGoogle(account.idToken!!)
+                //firebaseAuthWithGoogle(account.idToken!!)
+                val credential = GoogleAuthProvider.getCredential(account.idToken,null)
+                println("Debug: 3 ")
+                FirebaseAuth.getInstance().signInWithCredential(credential)
+                    .addOnCompleteListener{task->
+                        if(task.isSuccessful){
+
+                            val i  = Intent(this,MainActivity::class.java)
+                            startActivity(i)
+
+                        }else{
+                            Toast.makeText(this,task.exception?.message,Toast.LENGTH_SHORT).show()
+                        }
+
+                    }
             } catch (e: ApiException) {
                 // Google Sign In failed, update UI appropriately
                 Toast.makeText(this,"signInWithCredential:failure\n"+task.exception, Toast.LENGTH_SHORT).show()
@@ -186,55 +141,11 @@ class GoogleLoginActivity : AppCompatActivity() {
 
     private fun signIn() {
         val signInIntent = googleSignInClient.signInIntent
-        startActivityForResult(signInIntent, RC_SIGN_IN)
+        println("Debug: 4 ")
+        startActivityForResult(signInIntent, 10001)
+        println("Debug: 5 ")
        //launcher.launch(signInIntent)
     }
-
-//    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-//        result ->
-//        if (result.resultCode == Activity.RESULT_OK) {
-//            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
-//            handleResult(task)
-//        }
-//    }
-//
-//    private fun handleResult(task: Task<GoogleSignInAccount>) {
-//        if (task.isSuccessful) {
-//            val account: GoogleSignInAccount? = task.result
-//            if (account != null) {
-//                updateUI(account)
-//            }
-//        }
-//        else{
-//            Toast.makeText(this,task.exception.toString(), Toast.LENGTH_SHORT).show()
-//        }
-//    }
-
-    companion object {
-        private const val TAG = "GoogleActivity"
-        private const val RC_SIGN_IN = 9001
-    }
-
-//    private fun updateUI(account:GoogleSignInAccount) {
-//        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
-//        firebaseAuth.signInWithCredential(credential).addOnCompleteListener{
-//            if (it.isSuccessful) {
-//                // Sign in success, update UI with the signed-in user's information
-//                intent = Intent(this, MainActivity::class.java)
-//                val email = account.email.toString()
-//                val name = account.displayName.toString()
-//                val id = account.id.toString()
-//                val idToken = account.idToken.toString()
-//                intent.putExtra("email", email)
-//                intent.putExtra("name", name)
-//                intent.putExtra("id", id)
-//                intent.putExtra("idToken", idToken)
-//                println("Debug: email $email, name $name, id $id, idToken $idToken")
-//                updateDataBase(name,email,id,idToken)
-//                startActivity(intent)
-//            }
-//        }
-//    }
 
     private fun updateDataBase(name:String, email:String, pw:String, UID:String) {
         val query: String = "INSERT INTO users (user_name, email, pwd, fb_uid) VALUES ('$name','$email','$pw', '$UID')"
